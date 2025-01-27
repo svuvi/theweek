d4f5b3694d930046ee50f8ddb505d09711d909e5
Svuvi <61885927+Svuvi@users.noreply.github.com>
2025-01-27 09:45:32 +0100

    status code logging

--- middleware/logger.go
+++ middleware/logger.go
@@ -10,10 +10,24 @@ type Logger struct {
 	handler http.Handler
 }
 
+type responseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (rw *responseWriter) WriteHeader(statusCode int) {
+	rw.statusCode = statusCode
+	rw.ResponseWriter.WriteHeader(statusCode)
+}
+
 func (l *Logger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	start := time.Now()
-	l.handler.ServeHTTP(w, r)
-	log.Printf("%s %s %v", r.Method, r.URL.Path, time.Since(start))
+
+	rw := &responseWriter{ResponseWriter: w, statusCode: http.StatusOK}
+
+	l.handler.ServeHTTP(rw, r)
+
+	log.Printf("%d %s %s %v", rw.statusCode, r.Method, r.URL.Path, time.Since(start))
 }
 
 func NewLogger(handlerToWrap http.Handler) *Logger {
